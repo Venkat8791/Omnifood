86fa266df31114923c93e95badd50563d300981a
venkat gadiyakari <venkatgadiyakari@venkats-MacBook-Air.local>
2024-02-25 14:49:47 +0530

    implemented sticky header

--- js/script.js
+++ js/script.js
@@ -34,6 +34,27 @@ allLinks.forEach(function (link) {
 });
 
 ///////////////////////////////////////////////////////////
+// sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
